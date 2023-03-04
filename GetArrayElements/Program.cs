6d42efb0f12f85add98cb704163457ca98e5a659
Abvorobey <abvorobey@gmail.com>
2023-03-04 18:24:43 +0300

    Создание и заполнение массива без консоли

---
+++ GetArrayElements/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+using System;
+using static System.Console;
+Задайте двумерный массив размера m на n, каждый элемент
+в массиве находится по формуле: A[i, j] = i + j. 
+Выведите полученный массив на экран.
+
+m = 3, n = 4.
+
+0 1 2 3
+
+1 2 3 4
+
+2 3 4 5
+*/
+using System;
+using static System.Console;
+Clear();
+int [,] GetArray (int rows, int columns)
+{
+    int [,]array = new int [rows,columns];
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            array[i, j] = i+j;
+        }
+    }
+    return array;
+}
+void PrintArray (int [,]array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+PrintArray (GetArray (3, 4));
